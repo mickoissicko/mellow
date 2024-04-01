cf370370633715b2e2f112af0336c1503d16540f
mickoissicko <defualtusermick@gmail.com>
2024-04-01 12:53:59 +0200

    add function to get download directory path

--- src/fm.c
+++ src/fm.c
@@ -8,12 +8,12 @@
 
 void CreateTemp()
 {
-    system("mkdir ~/aur.gelper_tmp");
+    system("mkdir ~/mmgr/tmp");
 }
 
 void DeleteTemp()
 {
-    system("rm -rf ~/aur.gelper_tmp");
+    system("rm -rf ~/mmgr/tmp");
 }
 
 char* GetTempPath()
@@ -32,13 +32,39 @@ char* GetTempPath()
     TempPath = (char*)malloc(MAX_LEN);
 
     #ifdef _WIN32
-        snprintf(Buf, LEN, "%s\\mmgr.tmp", Path);
+        snprintf(Buf, LEN, "%s\\mmgr\\tmp", Path);
     #else
-        snprintf(Buf, LEN, "%s/mmgr.tmp", Path);
+        snprintf(Buf, LEN, "%s/mmgr/tmp", Path);
     #endif
 
     strcpy(TempPath, Buf); // cant return array
 
     return TempPath;
+}
+
+char* GetDownloadDir()
+{
+    #ifdef _WIN32
+        char* Path = Win32_Path();
+    #else
+        char* Path = Posix_Path();
+    #endif
+
+    unsigned long LEN = MAX_LEN;
+
+    char Buf[MAX_LEN];
+    char* DownloadDir;
+
+    DownloadDir = (char*)malloc(MAX_LEN);
+
+    #ifdef _WIN32
+        snprintf(Buf, LEN, "%s\\mmgr\\downloads", Path);
+    #else
+        snprintf(Buf, LEN, "%s/mmgr/downloads", Path);
+    #endif
+
+    strcpy(DownloadDir, Buf); // cant return array
+
+    return DownloadDir;
 }
 
