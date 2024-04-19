0c530acad3b4e27021106697865b3dffd654f6f1
mickoissicko <defualtusermick@gmail.com>
2024-04-19 13:21:33 +0200

    wget funcs and stuff

--- include/common.h
+++ include/common.h
@@ -2,11 +2,14 @@
     #define COMMON_H
 #endif
 
+#define NULL_TERM_SIZ 1
 #define MAX_SIZ 8192
 #define MAX_BUF 4096
 
 const static char MMGR[] = ".mix/mmgr";
 const static char MIX[] = ".mix";
+
+int Wget(char src[MAX_BUF + NULL_TERM_SIZ]);
 
 void Prepare();
 void Install();
