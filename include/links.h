9bb268ecc4626149c366c92d53385201d167d475
mickoissicko <defualtusermick@gmail.com>
2024-04-19 13:21:29 +0200

    add header containing links

---
+++ include/links.h
@@ -0,0 +1,10 @@
+#ifndef LINKS_H
+    #define LINKS_H
+#endif
+
+static char SHELL_SCRIPT[] =
+    "https://raw.githubusercontent.com/mickoissicko/mixmgr/main/pkgs/scripts/installer.sh";
+
+static char BATCH_SCRIPT[] =
+    "https://raw.githubusercontent.com/mickoissicko/mixmgr/main/pkgs/scripts/installer.bat";
+
