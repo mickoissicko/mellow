3d3ba736308a77553682353836c6520f6075a580
mickoissicko <defualtusermick@gmail.com>
2024-04-19 14:24:31 +0200

    pkg is cooler

--- src/wget.c
+++ src/wget.c
@@ -5,17 +5,17 @@
 #define TERMINATOR_SIZ 1
 #define MAX_LEN 4096
 
-int Wget(char src[MAX_LEN + TERMINATOR_SIZ])
+int Wget(const char pkg[MAX_LEN + TERMINATOR_SIZ])
 {
     int State = 0;
 
-    if (strlen(src) > MAX_LEN)
+    if (strlen(pkg) > MAX_LEN)
         return State = 1;
 
     printf("Connecting...\n");
     
     char Command[MAX_LEN];
-    snprintf(Command, sizeof(Command), "wget %s", src);
+    snprintf(Command, sizeof(Command), "wget %s", pkg);
 
     State = system(Command);
 
