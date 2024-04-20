89c648f887a1b3daf5a7cfecedde928513caba7d
mickoissicko <defualtusermick@gmail.com>
2024-04-20 14:36:30 +0200

    new constants and functions defined

--- include/common.h
+++ include/common.h
@@ -6,6 +6,7 @@
 #define MAX_SIZ 8192
 #define MAX_BUF 4096
 
+const static char AUR_PATH[] = "mmgr.aur";
 const static char LOCKFILE[] = "lock.pa";
 const static char INDEX[] = "index.msx";
 const static char LINKS[] = "links.msx";
@@ -14,12 +15,17 @@ const static char MIX[] = ".mix";
 
 int Wget(const char pkg[MAX_BUF + TERM_SIZ]);
 
+void CleanAURFolder();
+void MakeAURFolder();
+void ScriptRunner(char script[]);
 void Downloader(char* argv[]);
+void AURHelper(char *argv[]);
 void Prepare();
 void Install();
 void Reset();
 void Init();
 void Sync();
 void Help();
+void Git(char pkg[], char mode[]);
 void Get(const char* src);
 
