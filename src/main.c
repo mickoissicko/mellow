c803b6e24a078ea2dbb57d984e58bbf8671fae02
mickoissicko <defualtusermick@gmail.com>
2024-08-11 15:55:07 +0200

    add main

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include "../lib/gethome/gethome.h"
+#include "../include/imports.h"
+
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    MkDirs();
+
+    if (argc < 2)
+    {
+        printf("Read the wiki @ mick.gdn/wiki/mellow for help\n");
+        exit(1);
+    }
+
+    if (strcasecmp(argv[1], "-Ss") == 0 && argc > 2)
+    {
+        printf("Searching for '%s'...\n", argv[2]);
+
+        char* Home = gethome();
+        chdir(Home);
+
+        MkTmp(Home);
+        Scraper(argv[2]);
+    }
+
+    return 0;
+}
+
