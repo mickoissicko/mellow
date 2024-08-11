4483c164c42606633e05806a18f019dd00660fdd
mickoissicko <defualtusermick@gmail.com>
2024-08-12 00:44:02 +0200

    update imports

--- include/imports.h
+++ include/imports.h
@@ -5,7 +5,10 @@
 int Scraper(char* PackageName);
 int PrettyPrinter();
 
+void FormatFile(const char File[], const char FilePath[]);
+void DisplayResults(const char FilePath[]);
 void FiltreResults();
+void DelTmp();
 void MkDirs();
 void MkTmp();
 
