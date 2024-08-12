a73e7516dd188e515cc0eded7a4e1d44c6b5747e
mickoissicko <defualtusermick@gmail.com>
2024-08-12 20:53:39 +0200

    add new functions to import

--- include/imports.h
+++ include/imports.h
@@ -2,15 +2,14 @@
     #define IMPORTS_H
 #endif
 
+char* GetData(const int Val, const char File[], const char FilePath[]);
+
 void StripNewlines(const char File[], const char FilePath[]);
 void DisplayResults(const char FilePath[]);
 void FiltreResults();
 void DelTmp();
 void MkDirs();
 void MkTmp();
-
-char* FormatFileData(const char Data[]);
-char* GetFileData(const int Value);
 
 int Scraper(char* PackageName);
 int PrettyPrinter();
