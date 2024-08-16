19167b57d41bfc7d249f4fad5283634969f5e1f2
mickoissicko <defualtusermick@gmail.com>
2024-08-16 23:45:44 +0200

    add project files

---
+++ CMakeFiles/CLion.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CLion.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/mellow.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mickey/Documents/projects/CLion/src/dispresults.c" "CMakeFiles/mellow.dir/src/dispresults.c.o" "gcc" "CMakeFiles/mellow.dir/src/dispresults.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/git/clone.c" "CMakeFiles/mellow.dir/src/git/clone.c.o" "gcc" "CMakeFiles/mellow.dir/src/git/clone.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/json/filtrejson.c" "CMakeFiles/mellow.dir/src/json/filtrejson.c.o" "gcc" "CMakeFiles/mellow.dir/src/json/filtrejson.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/json/formatjson.c" "CMakeFiles/mellow.dir/src/json/formatjson.c.o" "gcc" "CMakeFiles/mellow.dir/src/json/formatjson.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/json/getdata.c" "CMakeFiles/mellow.dir/src/json/getdata.c.o" "gcc" "CMakeFiles/mellow.dir/src/json/getdata.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/json/prettyprint.c" "CMakeFiles/mellow.dir/src/json/prettyprint.c.o" "gcc" "CMakeFiles/mellow.dir/src/json/prettyprint.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/main.c" "CMakeFiles/mellow.dir/src/main.c.o" "gcc" "CMakeFiles/mellow.dir/src/main.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/scripts/build.c" "CMakeFiles/mellow.dir/src/scripts/build.c.o" "gcc" "CMakeFiles/mellow.dir/src/scripts/build.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/scripts/rmdeps.c" "CMakeFiles/mellow.dir/src/scripts/rmdeps.c.o" "gcc" "CMakeFiles/mellow.dir/src/scripts/rmdeps.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/scripts/rmpkgs.c" "CMakeFiles/mellow.dir/src/scripts/rmpkgs.c.o" "gcc" "CMakeFiles/mellow.dir/src/scripts/rmpkgs.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/scripts/uninstall.c" "CMakeFiles/mellow.dir/src/scripts/uninstall.c.o" "gcc" "CMakeFiles/mellow.dir/src/scripts/uninstall.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/splitfiles.c" "CMakeFiles/mellow.dir/src/splitfiles.c.o" "gcc" "CMakeFiles/mellow.dir/src/splitfiles.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/web/getpkg.c" "CMakeFiles/mellow.dir/src/web/getpkg.c.o" "gcc" "CMakeFiles/mellow.dir/src/web/getpkg.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/web/scraper.c" "CMakeFiles/mellow.dir/src/web/scraper.c.o" "gcc" "CMakeFiles/mellow.dir/src/web/scraper.c.o.d"
+  "/home/mickey/Documents/projects/CLion/src/files.cpp" "CMakeFiles/mellow.dir/src/files.cpp.o" "gcc" "CMakeFiles/mellow.dir/src/files.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
