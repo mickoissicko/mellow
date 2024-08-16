219f12d1891fdb61b1d95ca5fe43a8a35829cc67
mickoissicko <defualtusermick@gmail.com>
2024-08-16 18:29:18 +0200

    finish rmpkgs

--- src/scripts/rmpkgs.c
+++ src/scripts/rmpkgs.c
@@ -3,6 +3,18 @@
 
 void RemovePkgs(const char PKG_NAME[])
 {
-    printf("Removing pkg %s\n", PKG_NAME);
+    char RmCmd[2048];
+    int Err;
+
+    snprintf(RmCmd, sizeof(RmCmd), "sudo pacman -Rn '%s'", PKG_NAME);
+
+    printf("Removing package %s\n", PKG_NAME);
+    Err = system(RmCmd);
+
+    if (Err != 0)
+    {
+        printf("Something happened\n");
+        perror("@ ");
+    }
 }
 
