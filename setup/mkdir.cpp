951203901ec83f4c64325927ad34008e105d75c9
mickoissicko <defualtusermick@gmail.com>
2024-08-16 21:57:28 +0200

    add makedir function

---
+++ setup/mkdir.cpp
@@ -0,0 +1,25 @@
+#include <filesystem>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+
+namespace Fs = std::filesystem;
+
+void MkDir(const char DIR_NAME[], const char PATH[])
+{
+    if (chdir(PATH) != 0)
+    {
+        std::cout << "Could not change directory" << '\n';
+        exit(1);
+    }
+
+    if (Fs::exists(DIR_NAME))
+    {
+        std::cout << "Directory already exists" << '\n';
+        return;
+    }
+
+    else
+        Fs::create_directories(DIR_NAME);
+}
+
