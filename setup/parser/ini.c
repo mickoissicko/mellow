30ba3ff9b9b3c07e65b3314638e17b5923a2006b
mickoissicko <defualtusermick@gmail.com>
2024-08-17 18:17:20 +0200

    finish iniparser (barebones functionality)

--- setup/parser/ini.c
+++ setup/parser/ini.c
@@ -3,75 +3,93 @@
 #include <unistd.h>
 #include <string.h>
 
-int ParseIni(const char INI[], const char FULL_PATH[])
+#define MAX_LINE_LENGTH 8192
+
+char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE)
 {
     if (chdir(FULL_PATH) != 0)
     {
         printf("Error changing directory to `%s`\n", FULL_PATH);
         perror("An error occurred");
-        return 1;
+        exit(1);
     }
 
+    char* Line = (char*)malloc(sizeof(char) * 8192);
     FILE* IniCfg = fopen(INI, "r");
 
-    char CppStandard[1024];
-    char CppFallback[1024];
-    char CppCompiler[1024];
-    char CFallback[1024];
-    char CCompiler[1024];
-    char Compile[1024];
-    char Line[2048];
-
-    while(fgets(Line, sizeof(Line), IniCfg) != NULL)
+    while(fgets(Line, MAX_LINE_LENGTH, IniCfg) != NULL)
     {
         if (
             strstr(Line, "COMPILE = FALSE") != NULL ||
             strstr(Line, "COMPILE = TRUE")  != NULL
         ){
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(Compile, Line);
+
+            if (MODE == 0)
+                return Line;
         }
 
         if (strstr(Line, "FALLBACK_CXX") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(CppFallback, Line);
+
+            if (MODE == 1)
+                return Line;
         }
 
         if (strstr(Line, "FALLBACK_C") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(CFallback, Line);
+
+            if (MODE == 2)
+                return Line;
         }
 
         if (strstr(Line, "CXX_STANDARD") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(CppStandard, Line);
+
+            if (MODE == 3)
+                return Line;
         }
 
         if (strstr(Line, "C_COMPILER") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(CCompiler, Line);
+
+            if (MODE == 4)
+                return Line;
         }
 
         if (strstr(Line, "CXX_COMPILER") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
-            strcpy(CppCompiler, Line);
+
+            if (MODE == 5)
+                return Line;
+        }
+
+        if (
+            strstr(Line, "FALLBACK_PATH") == NULL &&
+            strstr(Line, "PATH")          != NULL
+        ){
+            Line[strcspn(Line, "\n")] = 0;
+
+            if (MODE == 6)
+                return Line;
+        }
+
+        if (
+            strstr(Line, "FALLBACK_PATH") != NULL
+        ){
+            Line[strcspn(Line, "\n")] = 0;
+
+            if (MODE == 7)
+                return Line;
         }
     }
 
     fclose(IniCfg);
-
-    printf("%s\n", Compile);
-    printf("%s\n", CppFallback);
-    printf("%s\n", CFallback);
-    printf("%s\n", CppStandard);
-    printf("%s\n", CppCompiler);
-    printf("%s\n", CCompiler);
-
-    return 0;
+    return Line;
 }
 
