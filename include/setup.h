e4a50e93ed04989eefea21134547729450bb4d48
mickoissicko <defualtusermick@gmail.com>
2024-08-17 20:01:31 +0200

    add function defs

--- include/setup.h
+++ include/setup.h
@@ -1,9 +1,11 @@
 #ifndef SETUP_H
 #define SETUP_H
+#include <stdio.h>
 
 void Translate(const char GLOBAL[], const char USER[], const char FULL_PATH[]);
 void MkDir(const char DIR_NAME[], const char PATH[]);
 void Curl(const char URL[], const char FILE_NAME[]);
+void WriteFile(FILE* File, char Line[]);
 void RmDir(const char DIR_NAME[]);
 
 char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE);
