73bc034f182b40b4345565cfe81a5f4f56272f00
mickoissicko <defualtusermick@gmail.com>
2024-08-17 16:52:48 +0200

    add new fns

--- include/setup.h
+++ include/setup.h
@@ -3,6 +3,9 @@
 
 void MkDir(const char DIR_NAME[], const char PATH[]);
 void Curl(const char URL[], const char FILE_NAME[]);
+void RmDir(const char DIR_NAME[]);
+
+int ParseIni(const char INI[], const char FULL_PATH[]);
 
 #endif // SETUP_H
 
