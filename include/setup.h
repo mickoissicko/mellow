de45108e0457e610c561ec9d1be526fc57f96ae1
mickoissicko <defualtusermick@gmail.com>
2024-08-18 01:03:04 +0200

    add new fn defs

--- include/setup.h
+++ include/setup.h
@@ -2,13 +2,19 @@
 #define SETUP_H
 #include <stdio.h>
 
-void Translate(const char GLOBAL[], const char USER[], const char FULL_PATH[]);
+void InstallPackages(const char PACKAGE_LIST[], const char PATH[]);
+void Translate(const char GLOBAL[], const char FULL_PATH[]);
+void GetFields(const char FILE_NAME[], const char PATH[]);
 void MkDir(const char DIR_NAME[], const char PATH[]);
 void Curl(const char URL[], const char FILE_NAME[]);
 void WriteFile(FILE* File, char Line[]);
 void RmDir(const char DIR_NAME[]);
 
-char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE);
+char* FormatField(const char FILE_NAME[], const char PATH[]);
+char* ParseIni(const char INI[], const char FULL_PATH[]);
+char* FindPackages(char PackageList[]);
+
+int CheckInstalled(char PackageList[], const char PATH[]);
 
 #endif // SETUP_H
 
