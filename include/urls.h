850435b47b2460a83a4fec678e4deaa588a52925
mickoissicko <defualtusermick@gmail.com>
2024-08-18 01:02:52 +0200

    update urls

--- include/urls.h
+++ include/urls.h
@@ -1,7 +1,6 @@
 #ifndef URLS_H
 #define URLS_H
 
-const static char GLOBAL_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/global.ini";
-const static char USR_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/usr.ini";
+const static char PACKAGE_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/packages.ini";
 
 #endif // URLS_H
