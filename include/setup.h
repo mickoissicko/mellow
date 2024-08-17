1813b117f502f8737c1ff0c65371654117d31acd
mickoissicko <defualtusermick@gmail.com>
2024-08-17 18:17:37 +0200

    new fnc

--- include/setup.h
+++ include/setup.h
@@ -1,11 +1,12 @@
 #ifndef SETUP_H
 #define SETUP_H
 
+void Translate(const char GLOBAL[], const char USER[], const char FULL_PATH[]);
 void MkDir(const char DIR_NAME[], const char PATH[]);
 void Curl(const char URL[], const char FILE_NAME[]);
 void RmDir(const char DIR_NAME[]);
 
-int ParseIni(const char INI[], const char FULL_PATH[]);
+char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE);
 
 #endif // SETUP_H
 
