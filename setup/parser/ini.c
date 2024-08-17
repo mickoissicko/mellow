d05d2921f4b6a8ffeab7149eb13af009e55772ac
mickoissicko <defualtusermick@gmail.com>
2024-08-18 01:03:36 +0200

    update ini.c

--- setup/parser/ini.c
+++ setup/parser/ini.c
@@ -5,7 +5,7 @@
 
 #define MAX_LINE_LENGTH 8192
 
-char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE)
+char* ParseIni(const char INI[], const char FULL_PATH[])
 {
     if (chdir(FULL_PATH) != 0)
     {
@@ -19,87 +19,15 @@ char* ParseIni(const char INI[], const char FULL_PATH[], const int MODE)
 
     while(fgets(Line, MAX_LINE_LENGTH, IniCfg) != NULL)
     {
-        if (
-            strstr(Line, "COMPILE = FALSE") != NULL ||
-            strstr(Line, "COMPILE = TRUE")  != NULL
-        ){
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 0)
-                return Line;
-        }
-
-        if (strstr(Line, "FALLBACK_CXX") != NULL)
-        {
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 1)
-                return Line;
-        }
-
-        if (
-            strstr(Line, "FALLBACK_CXX") == NULL &&
-            strstr(Line, "FALLBACK_C")   != NULL
-        ){
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 2)
-                return Line;
-        }
-
-        if (strstr(Line, "CXX_STANDARD") != NULL)
-        {
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 3)
-                return Line;
-        }
-
-        if (strstr(Line, "C_COMPILER") != NULL)
-        {
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 4)
-                return Line;
-        }
-
-        if (strstr(Line, "CXX_COMPILER") != NULL)
-        {
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 5)
-                return Line;
-        }
-
         if (strstr(Line, "REQUIRED") != NULL)
         {
             Line[strcspn(Line, "\n")] = 0;
 
-            if (MODE == 6)
-                return Line;
-        }
-
-        if (
-            strstr(Line, "FALLBACK_PATH") == NULL &&
-            strstr(Line, "PATH")          != NULL
-        ){
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 7)
-                return Line;
-        }
-
-        if (
-            strstr(Line, "FALLBACK_PATH") != NULL
-        ){
-            Line[strcspn(Line, "\n")] = 0;
-
-            if (MODE == 8)
-                return Line;
+            return Line;
         }
     }
 
     fclose(IniCfg);
-    return Line;
+    return 0;
 }
 
