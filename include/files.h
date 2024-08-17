6dc750ab0d2d530cf7006503a5a84dfe853b9db2
mickoissicko <defualtusermick@gmail.com>
2024-08-18 01:04:01 +0200

    add new files

---
+++ include/files.h
@@ -0,0 +1,9 @@
+#ifndef FILES_H
+#define FILES_H
+
+const static char PACKAGE_RAW[] = "packages.raw";
+const static char PACKAGES[] = "packages.ini";
+const static char SETUP_TMP[] = "mellow";
+
+#endif //FILES_H
+
