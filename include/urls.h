a79711b247a44179bf84d1f9593f87ba0a413c1d
mickoissicko <defualtusermick@gmail.com>
2024-08-17 16:52:44 +0200

    add urls

--- include/urls.h
+++ include/urls.h
@@ -1,7 +1,7 @@
 #ifndef URLS_H
 #define URLS_H
 
-const static char PKG_INDEX[] = "";
+const static char GLOBAL_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/global.ini";
+const static char USR_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/usr.ini";
 
 #endif // URLS_H
-
