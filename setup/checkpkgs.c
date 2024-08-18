350c67fd8aaf954adb2e1d91e5ff41609b869498
mickoissicko <defualtusermick@gmail.com>
2024-08-18 18:13:05 +0200

    finish function

--- setup/checkpkgs.c
+++ setup/checkpkgs.c
@@ -4,17 +4,41 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-char* FindPackages(char PackageList[])
+#define MAX_LINE_LENGTH 1024
+
+char* FindPackages(char PackageList[], int LineCount, FILE* InstalledPackages)
 {
+    rewind(InstalledPackages);
+
     char* MissingPackageList = (char*)malloc(sizeof(char) * strlen(PackageList) + 1);
+    char Line[MAX_LINE_LENGTH];
 
+    int CurrentLine = 0;
 
+    while (fgets(Line, sizeof(Line), InstalledPackages) != NULL)
+    {
+        char* LinePtr;
 
+        if ((LinePtr = strchr(Line, ' ')) != NULL)
+            *LinePtr = '\0';
+
+        if (CurrentLine == LineCount)
+        {
+            strcpy(MissingPackageList, Line);
+            return MissingPackageList;
+        }
+
+        CurrentLine++;
+    }
+
+    printf("%s\n", MissingPackageList);
     return MissingPackageList;
 }
 
 int CheckInstalled(char PackageList[], const char PATH[])
 {
+    int Missing = 0;
+
     if (chdir(PATH) != 0)
     {
         perror("Could not change directory");
@@ -30,11 +54,15 @@ int CheckInstalled(char PackageList[], const char PATH[])
     dup2(Results, STDOUT_FILENO);
     close(Results);
 
-    system(Buffer);
+    if (system(Buffer) != 0)
+        Missing = 1;
 
     dup2(StdOut, STDOUT_FILENO);
     close(StdOut);
 
-    return 0;
+    if (!Missing)
+        return 0;
+
+    return 1;
 }
 
