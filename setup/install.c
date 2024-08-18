b9b33393b228b7502dab4e0785fc68df0eb72386
mickoissicko <defualtusermick@gmail.com>
2024-08-18 18:13:30 +0200

    implement missing package checker (not done yet)

--- setup/install.c
+++ setup/install.c
@@ -1,26 +1,66 @@
+#include "../include/setup.h"
+
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdio.h>
 
-#include "../include/setup.h"
+#define MAX_PACKAGE_LENGTH 1024
+#define MAX_PACKAGE_COUNT 10
+
+void InstallMissingPackages(char PackageList[], char InstalledPackages[], int Index)
+{
+    char* FixedSpace = Append(InstalledPackages);
+    char* MissingPackages[MAX_PACKAGE_COUNT];
+
+    int Delim = ' ';
+
+    MissingPackages[Index] = NULL;
+
+    char* Start = FixedSpace;
+    char* End = strchr(FixedSpace, Delim);
+
+    if (End == NULL)
+        MissingPackages[Index] = FixedSpace;
+
+    else if (Start == End)
+        MissingPackages[Index] = End + 1;
+
+    else
+    {
+        int Length = End - Start;
+
+        MissingPackages[Index] = strndup(Start, Length);
+    }
+
+    printf("%s\n", MissingPackages[Index]);
+}
 
 void InstallPackages(const char PACKAGE_LIST[], const char PATH[])
 {
-    char* PackageList = (char*)malloc(sizeof(char) * strlen(PACKAGE_LIST) + 1);
-    strcpy(PackageList, PACKAGE_LIST);
-
-    printf("Checking for installed packages...\n");
-
-    if (CheckInstalled(PackageList, PATH) == 0)
+    if (chdir(PATH) != 0)
     {
-        printf("All dependencies met!\n");
-        return;
+        perror("Could not change directory");
+        exit(1);
     }
+
+    char* PackageList = (char*)malloc(sizeof(char) * strlen(PACKAGE_LIST) + 1024);
+    strcpy(PackageList, PACKAGE_LIST);
 
     printf("Installing packages...\n");
 
-    char* MissingPackages = FindPackages(PackageList);
+    int TotalLines = 0;
+    FILE* Installed = fopen("pacman.txt", "r");
 
-    printf("%s\n", MissingPackages);
+    for (int Cursor = getc(Installed); Cursor != EOF; Cursor = getc(Installed))
+        if (Cursor == '\n')
+        {
+            char* InstalledPackages = FindPackages(PackageList, TotalLines, Installed);
+            InstallMissingPackages(PackageList, InstalledPackages, TotalLines);
+
+            TotalLines++;
+        }
+
+    fclose(Installed);
 }
 
