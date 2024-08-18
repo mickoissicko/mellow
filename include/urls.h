74ab066bfd3b9fd2ee754b43109ef7117b8fcfd1
mickoissicko <defualtusermick@gmail.com>
2024-08-18 18:13:44 +0200

    update url to pt to gitlab mirror

--- include/urls.h
+++ include/urls.h
@@ -1,6 +1,6 @@
 #ifndef URLS_H
 #define URLS_H
 
-const static char PACKAGE_INI[] = "https://raw.githubusercontent.com/mickoissicko/mellow-mirror/main/mirror/packages.ini";
+const static char PACKAGE_INI[] = "https://gitlab.com/fmirror/mirror1/-/raw/main/packages/packages.ini";
 
 #endif // URLS_H
