4517ed8e1a9471161749b5037bbad5393b8f7a38
mickoissicko <defualtusermick@gmail.com>
2024-08-21 19:33:12 +0200

    fix rmdir

--- setup/rmdir.cpp
+++ setup/rmdir.cpp
@@ -1,13 +1,13 @@
 #include <filesystem>
 
-extern "C"
-{
-    void RmDir(const char DIRECTORY[]);
-}
-
 namespace Fs = std::filesystem;
 
-void RmDir(const char DIRECTORY[])
+extern "C"
+{
+    void RmDir(const char* DIRECTORY);
+}
+
+void RmDir(const char* DIRECTORY)
 {
     if (Fs::exists(DIRECTORY))
         Fs::remove_all(DIRECTORY);
