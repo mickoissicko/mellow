7af8bcb1f979fe5364235cea13e1cb3ce707ab3a
mickoissicko <defualtusermick@gmail.com>
2024-08-21 19:33:21 +0200

    add readfile function

--- setup/parser/readfile.c
+++ setup/parser/readfile.c
@@ -1,13 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #define MAX_LINE_LENGTH 1024
 
-char* ReadFile(FILE* File)
+char* ReadFile(FILE* File, int Mode)
 {
-    char* Line = 0;
+    char* Line = (char*)malloc(sizeof(char) * MAX_LINE_LENGTH);
 
-    //
+    if (fgets(Line, MAX_LINE_LENGTH, File) != NULL)
+    {
+        if (Mode == 1)
+        {
+            Line[strcspn(Line, "\n")] = 0;
+            return Line;
+        }
 
-    return Line;
+        return Line;
+    }
+
+    return "read.fail";
 }
 
