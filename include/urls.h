a18ebb28578369a8f87685e868df29b190603a5a
mickoissicko <defualtusermick@gmail.com>
2024-08-21 19:32:07 +0200

    update urls

--- include/urls.h
+++ include/urls.h
@@ -1,6 +1,7 @@
 #ifndef URLS_H
 #define URLS_H
 
+const static char ARCHLINUX_REPOSITORY[] = "https://gitlab.archlinux.org/archlinux/packaging/packages/";
 const static char PACKAGE_INI[] = "https://gitlab.com/fmirror/mirror1/-/raw/main/packages/packages.ini";
 
 #endif // URLS_H
