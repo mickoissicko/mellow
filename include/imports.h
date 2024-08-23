6d3c762fc208bacaeec7a21e063653a7088e4d35
mickoissicko <defualtusermick@gmail.com>
2024-08-23 23:46:32 +0200

    fix formatting + remove unneeded fn

--- include/imports.h
+++ include/imports.h
@@ -1,5 +1,6 @@
 #ifndef IMPORTS_H
 #define IMPORTS_H
+
 #include <stdio.h>
 
 char* FindPackages(char PackageList[], int LineCount, FILE* InstalledPackages);
@@ -11,15 +12,14 @@ char* ReadFile(FILE* File, int Mode);
 char* Append(char String[]);
 
 void MakeInstallationList(char PackageList[], char InstalledPackageList[], const char PATH[]);
+void StripNewlines(const char FILE_NAME[], const char FILE_PATH[]);
 void InstallPackages(const char PACKAGE_LIST[], const char PATH[]);
 void Translate(const char GLOBAL[], const char FULL_PATH[]);
 void GetFields(const char FILE_NAME[], const char PATH[]);
-void DownloadPackage(char Package[], const char PATH[]);
-
-void StripNewlines(const char FILE_NAME[], const char FILE_PATH[]);
 void BuildPackage(char PackageName[], const char PATH[]);
-void RemoveDeps(char PackageName[], const char PATH[]);
+void DownloadPackage(char Package[], const char PATH[]);
 void SplitFiles(const int INDEX, const int START_LINE);
+void RemoveDeps(char PackageName[], const char PATH[]);
 void MkDir(const char DIR_NAME[], const char PATH[]);
 void Curl(const char URL[], const char FILE_NAME[]);
 void MakePackage(char Package[], const char PATH[]);
